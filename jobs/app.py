14230124f10a45c3ef8ef6a727b4de89cd3d6e0e
corvotorvo <83085402+MAlcont@users.noreply.github.com>
2022-07-02 09:41:18 +0200

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -22,11 +22,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
         cursor.close
         return results
 
-@app.teardown_appcontext
-def close_connection(excpetion):
-    connection = getattr(g, '_connection', None)
-    if connection = None:
-        connection.close()
+
 
 
 
