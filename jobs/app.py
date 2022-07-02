31530ed782c8118e7e66c8c4036ec218ea1e9798
corvotorvo <83085402+MAlcont@users.noreply.github.com>
2022-07-02 09:38:07 +0200

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -15,6 +15,20 @@ def open_connection():
 def execute_sql(sql, values = (), commit = False, single = False):
     connection = open_connection()
     cursor = execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+        cursor.close
+        return results
+
+@app.teardown_appcontext
+def close_connection(excpetion):
+    connection = getattr(g, '_connection', None)
+    if connection = None:
+        connection.close
+
+
 
 
 @app.route("/")
